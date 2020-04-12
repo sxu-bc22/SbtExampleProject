3cad9aeeb4ad6f7ef8f99f3f5bd2bd296dc99d98
sxu-bc22 <stephen.g.xu@gmail.com>
2020-04-12 14:47:36 -0400

    0412-b

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -3,5 +3,11 @@ package example
 object Main extends App {
   val ages = Seq(42, 621, 29, 60)
   println(s"The oldest person is ${ages.max}")
+
+  var m = List(1,2,3,4,5,6).map(x =>x*2)
+  println(m.toString())
+
+  var f = m.filter(_/2%2 == 0)
+  println(f.toString())
 }
 
--- ws.sc
+++ ws.sc
@@ -1,2 +1,3 @@
 var m = List(1,2,3,4,5,6).filter(_ % 2 == 0).map(x =>x*2)
 
+def a = Array[Int]()
