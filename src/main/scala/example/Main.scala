3a9dde824d0db38e3907daf7ebb1e0132d32b77c
sxu-bc22 <stephen.g.xu@gmail.com>
2020-04-13 20:46:39 -0400

    0413-b

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -4,10 +4,27 @@ object Main extends App {
   lazy val ages = Seq(42, 621, 29, 60)
   println(s"The oldest person is ${ages.max}")
 
-  var m = List(1,2,3,4,5,6).map(x =>x*2)
+  var m = List(1,2,3,4,5,6).map(_*2)
   println(m.toString())
 
-  var f = m.filter(_/2%2 == 0)
+  var f = m.filter(y => (y/2)%2 == 0)
   println(f.toString())
+
+  def foo(x: Int): Unit = { println("foo : "+ x) }
+  foo(18+2)
+
+  val t = new Test
+  t.age = -5
+  println(t.age)
+
+
+  class Test {
+    private var a = 0
+    def age: Int = a
+    def age_=(n:Int): Unit = {
+     // require(n>0)
+      a = n
+    }
+  }
 }
 
