908f4ae88de5988ee204576f255b57e4401a2e56
sxu-bc22 <stephen.g.xu@gmail.com>
2020-04-15 15:34:05 -0400

    0415-a

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,6 +1,28 @@
 package example
 
+import java.util.Date
+import scala.math._
+
+
 object Main extends App {
+  def log(date : Date, message : String) = {
+    println(date + " : " + message);
+  }
+
+  val date = new Date();
+  val newLog = log(date, _ : String)
+  newLog("msg 1")
+  newLog("msg 2")
+
+  // high order function
+  def math(x: Double, y: Double, f: (Double, Double) => Double) : Double= f(x, y)
+  println("Math "+math(30.0, 60.0, (x,y)=>sin(x*3.1415926535/180.0)+cos(y*3.1415926535/180.0)))
+
+
+  def add(x : Int = 20, y : Int = 10) : Int  = x + y
+
+  println(add(5,30))
+
   lazy val ages = Seq(42, 621, 29, 60)
   println(s"The oldest person is ${ages.max}")
 
@@ -26,5 +48,9 @@ object Main extends App {
       a = n
     }
   }
+
+
+
+
 }
 
